b8daa3fc02d3db500d2ec62bf1d344361e9ad188
Hugo Simões <hugosimoescontato@gmail.com>
2025-06-06 19:06:44 -0300

    feat: add password encrypted

--- src/main/java/dev/resume/backend/usecases/CreateUserUseCase.java
+++ src/main/java/dev/resume/backend/usecases/CreateUserUseCase.java
@@ -3,17 +3,17 @@ package dev.resume.backend.usecases;
 import dev.resume.backend.entities.UserEntity;
 import dev.resume.backend.exceptions.UserAlreadyExistException;
 import dev.resume.backend.repositories.UserRepository;
-//import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreateUserUseCase {
     private final UserRepository userRepository;
-//    private final PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
-    public CreateUserUseCase(UserRepository userRepository) {
+    public CreateUserUseCase(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
-//        this.passwordEncoder = passwordEncoder;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public UserEntity execute(UserEntity userEntity){
@@ -23,8 +23,8 @@ public class CreateUserUseCase {
             throw new UserAlreadyExistException();
         }
 
-//        var password = passwordEncoder.encode(userEntity.getPassword());
-//        userEntity.setPassword(password);
+        var password = passwordEncoder.encode(userEntity.getPassword());
+        userEntity.setPassword(password);
 
         return this.userRepository.save(userEntity);
     }
