13c07d3298940a9a00548a7c56c05c32b00073ac
Hugo Simoes <58408758+hugosimoesdev@users.noreply.github.com>
2025-06-19 20:59:08 -0300

    ref: add cors configuration and auth controller

--- src/main/java/dev/resume/backend/controller/AuthController.java
+++ src/main/java/dev/resume/backend/controller/AuthController.java
@@ -1,7 +1,11 @@
 package dev.resume.backend.controller;
 
 import dev.resume.backend.domain.dto.AuthUserDTO;
+import dev.resume.backend.domain.entity.UserEntity;
 import dev.resume.backend.service.AuthUserUseCase;
+import dev.resume.backend.service.CreateUserUseCase;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,17 +16,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
     private final AuthUserUseCase authUserUseCase;
+    private final CreateUserUseCase createUserUseCase;
 
-
-    public AuthController(AuthUserUseCase authUserUseCase) {
+    public AuthController(AuthUserUseCase authUserUseCase, CreateUserUseCase createUserUseCase)  {
         this.authUserUseCase = authUserUseCase;
+        this.createUserUseCase = createUserUseCase;
     }
 
-    @PostMapping
+    @PostMapping("/signin")
     public ResponseEntity<Object> create(@RequestBody AuthUserDTO authUserDTO) {
         try {
             var result = authUserUseCase.execute(authUserDTO);
             return ResponseEntity.ok(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<Object> create(@Valid @RequestBody UserEntity userEntity){
+        try {
+            var result = this.createUserUseCase.execute(userEntity);
+            return ResponseEntity.status(HttpStatus.CREATED).body(result);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
